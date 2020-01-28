6697f8993b032ee139efa927295267e8925d2e29
Hoshimotao <jthomas302@gmail.com>
2020-01-27 19:27:14 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+     <Header title="Guess a Number"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
